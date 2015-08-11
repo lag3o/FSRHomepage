01c712b10ad3ac0d8273febad49cb0a936044d2e
lag3o <lag_dab@hotmail.de>
2015-08-11 19:01:11 +0200

    db, angular and bootstrap

--- FSRHomepage/config/application.rb
+++ FSRHomepage/config/application.rb
@@ -11,7 +11,9 @@ module FSRHomepage
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
